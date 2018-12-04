14921788cf773195336023ee7dc18992f9855660
renato <renao_py@outlook.com>
2018-12-04 14:43:38 +0800

    Signed-off-by: renato <renao_py@outlook.com>

--- child/test.java
+++ child/test.java
@@ -1,4 +1,11 @@
 public class test{
+	static
+	{
+		System.out.println("this method is a Initializer,do not need instantiate \"test\" class.");
+	}
+	{
+		System.out.println("this method is a method,need instantiate \"test\" class.");
+	}
 	public static void main(String[] args){
 		System.out.println("this is a test.");
 	}
